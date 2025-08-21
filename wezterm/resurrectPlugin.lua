c04a28caacd8cf61a0ca00e8d9451298bfe22835
unknown <tenbagger89@gmail.com>
2025-08-21 23:50:11 +0900

    250821

--- wezterm/resurrectPlugin.lua
+++ wezterm/resurrectPlugin.lua
@@ -6,8 +6,8 @@ local resurrect = wezterm.plugin.require("https://github.com/MLFlexer/resurrect.
 -- 플러그인 위치
 -- C:\Users\jinpyo\AppData\Roaming\wezterm\plugins\httpssCssZssZsgithubsDscomsZsMLFlexersZsresurrectsDswezterm\plugin\resurrect
 
-local utils = require("resurrect.utils")
-local pane_tree_mod = require("resurrect.pane_tree")
+-- local utils = require("resurrect.utils")
+-- local pane_tree_mod = require("resurrect.pane_tree")
 -- wezterm.on("gui-startup", resurrect.state_manager.resurrect_on_gui_startup)
 
 local logger = wezterm.plugin.require("https://github.com/sei40kr/wez-logging.git")
@@ -24,14 +24,46 @@ return {
   {
     key = "s",
     mods = "ALT",
-    action = wezterm.action.Multiple {
-      resurrect.tab_state.save_tab_action(),
-      wezterm.action_callback(function(window, pane)
-        window:toast_notification("Resurrect", "✅ 탭 저장 완료!", nil, 1)
-      end),
-    },
+    action = resurrect.tab_state.save_tab_action()
+    -- action = wezterm.action.Multiple {
+    --   resurrect.tab_state.save_tab_action(),
+    --   wezterm.action_callback(function(window, pane)
+    --     window:toast_notification("Resurrect", "✅ 탭 저장 완료!", nil, 1)
+    --   end),
+    -- },
   },
-{
+-- {
+--     key = "r",
+--     mods = "ALT",
+--     action = wezterm.action_callback(function(win, pane)
+--       resurrect.fuzzy_loader.fuzzy_load(win, pane, function(id, label)
+--         local type = string.match(id, "^([^/]+)") -- match before '/'
+--         id = string.match(id, "([^/]+)$") -- match after '/'
+--         id = string.match(id, "(.+)%..+$") -- remove file extention
+--         local opts = {
+--           relative = true,
+--           restore_text = true,
+--           close_open_panes = true, -- 복원 전에 현재 탭의 모든 pane을 닫고 복원할 pane만 유지함. tab 복원 시 유용.
+--           on_pane_restore = resurrect.tab_state.default_on_pane_restore,
+--         }
+--         if type == "workspace" then
+--           local state = resurrect.state_manager.load_state(id, "workspace")
+--           resurrect.workspace_state.restore_workspace(state, opts)
+--           win:toast_notification("Resurrect", "🔄 워크스페이스 복원 완료: " .. id, nil, 1)
+--         elseif type == "window" then
+--           local state = resurrect.state_manager.load_state(id, "window")
+--           resurrect.window_state.restore_window(pane:window(), state, opts)
+--                       win:toast_notification("Resurrect", "🪟 윈도우 복원 완료: " .. id, nil, 1)
+--         elseif type == "tab" then
+--           local state = resurrect.state_manager.load_state(id, "tab")
+--           resurrect.tab_state.restore_tab(pane:tab(), state, opts)
+--                       win:toast_notification("Resurrect", "📑 탭 복원 완료: " .. id, nil, 1)
+--         end
+--       end)
+--     end),
+--   },
+
+    {
     key = "r",
     mods = "ALT",
     action = wezterm.action_callback(function(win, pane)
@@ -42,21 +74,18 @@ return {
         local opts = {
           relative = true,
           restore_text = true,
-          close_open_panes = true, -- 복원 전에 현재 탭의 모든 pane을 닫고 복원할 pane만 유지함. tab 복원 시 유용.
           on_pane_restore = resurrect.tab_state.default_on_pane_restore,
+          close_open_panes = true, -- 복원 전에 현재 탭의 모든 pane을 닫고 복원할 pane만 유지함. tab 복원 시 유용.
         }
         if type == "workspace" then
           local state = resurrect.state_manager.load_state(id, "workspace")
           resurrect.workspace_state.restore_workspace(state, opts)
-          win:toast_notification("Resurrect", "🔄 워크스페이스 복원 완료: " .. id, nil, 1)
         elseif type == "window" then
           local state = resurrect.state_manager.load_state(id, "window")
           resurrect.window_state.restore_window(pane:window(), state, opts)
-                      win:toast_notification("Resurrect", "🪟 윈도우 복원 완료: " .. id, nil, 1)
         elseif type == "tab" then
           local state = resurrect.state_manager.load_state(id, "tab")
           resurrect.tab_state.restore_tab(pane:tab(), state, opts)
-                      win:toast_notification("Resurrect", "📑 탭 복원 완료: " .. id, nil, 1)
         end
       end)
     end),
